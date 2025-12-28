28c395a7d0994f51d8aa6a6bce10878657b38760
muhaj-dev <muhammodholawale@gmail.com>
2025-12-28 16:40:08 +0100

    implement the about page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { ScrollTrigger, SplitText } from 'gsap/all'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Cocktails from './components/Cocktails.jsx'
+import About from './components/About.jsx'
+
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -13,6 +15,7 @@ const App = () => {
       <Navbar />
       <Hero />
        <Cocktails />
+       <About />
     </main>
   )
 }
